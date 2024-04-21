1f433c76389700e4c56e075fd6a4f5abc77200db
Esakki Muthu <esakkimuthu6842@gmail.com>
2024-04-21 16:50:48 +0530

    added mini-redis

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Connect to the mini-redis server
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // Set a key-value pair
+    client.set("hello", "world".into()).await?;
+
+    // Get the value using the key
+    let result = client.get("hello").await?;
+
+    // Print the result stored in mini redis
+    println!("<------------>");
+    println!();
+    println!("The result stored in mini redis is {:?}", result);
+    println!();
+    println!("<------------>");
+    Ok(())
 }
